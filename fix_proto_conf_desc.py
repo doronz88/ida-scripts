d27eb68a105187e72de68124ea56670d941b2176
0xepsil0n <areallyformailmail7@proton.me>
2024-12-10 21:39:26 +0100

    fixes and code cleanup

--- fix_proto_conf_desc.py
+++ fix_proto_conf_desc.py
@@ -6,119 +6,157 @@ import ida_name
 from ida_nalt import REF_OFF32, REFINFO_SIGNEDOP, REFINFO_SELFREF
 from idc import FF_DATA, FF_DWORD
 
-class PCDStruct:
-    tif = 0
-    size = 0
-    PCD_IDA_STRUCT_TYPE_NAME = "ProtConfDescriptor"
+class CStruct: 
 
-    def _create_pcd_struct(self):
-        SELFREF = REFINFO_SIGNEDOP | REFINFO_SELFREF
-        tif = ida_typeinf.tinfo_t()
-        udt = ida_typeinf.udt_type_data_t()
+    def append_int16_member(self, name):
         udm = ida_typeinf.udm_t()
+        udm.name = name
+        udm.offset = self.tif.get_unpadded_size() * 8
+        udm.type = ida_typeinf.tinfo_t(ida_typeinf.BT_INT16)
+        udm.size = udm.type.get_size() * 8
+        self.tif.add_udm(udm)
 
-        tif.create_udt(udt)
-        tif.set_named_type(None, self.PCD_IDA_STRUCT_TYPE_NAME)
-        udm.name = "ProtocolDescriptor"
-        udm.offset = 0
+    def append_int32_member(self, name):
+        udm = ida_typeinf.udm_t()
+        udm.name = name
+        udm.offset = self.tif.get_unpadded_size() * 8
+        udm.type = ida_typeinf.tinfo_t(ida_typeinf.BT_INT32)
+        udm.size = udm.type.get_size() * 8
+        self.tif.add_udm(udm)
+
+    def append_reloffset_member(self, name):
+        udm = ida_typeinf.udm_t()
+        udm.name = name
+        udm.offset = self.tif.get_unpadded_size() * 8
         udm.type = ida_typeinf.tinfo_t(ida_typeinf.BT_INT32)
         udm.size = udm.type.get_size() * 8
         udm.repr.set_vtype(ida_typeinf.FRB_OFFSET)
-        udm.repr.ri.init(ida_nalt.REF_OFF32 | SELFREF)
-        tif.add_udm(udm)
+        udm.repr.ri.init(ida_nalt.REF_OFF32 | self.SELFREF)
+        self.tif.add_udm(udm)
 
-        udm.name = "NominalTypeDescriptor"
-        udm.offset = tif.get_unpadded_size() * 8
-        tif.add_udm(udm)
-
-        udm.name = "PWT"
-        udm.offset = tif.get_unpadded_size() * 8
-        tif.add_udm(udm)
-
-        udm = ida_typeinf.udm_t()
-        udm.name = "Flags"
-        udm.offset = tif.get_unpadded_size() * 8
-        udm.type = ida_typeinf.tinfo_t(ida_typeinf.BT_INT32)
-        udm.size = udm.type.get_size() * 8
-        tif.add_udm(udm)
-        return tif
-
-    def __init__(self):  
-        #Delete existing PCD struct
-        tif = ida_typeinf.tinfo_t()
-        if tif.get_named_type(None, self.PCD_IDA_STRUCT_TYPE_NAME):
-            ida_typeinf.del_named_type(None, self.PCD_IDA_STRUCT_TYPE_NAME, ida_typeinf.NTF_TYPE)
-
-        self.tif = self._create_pcd_struct()
-        self.size = 4*4
-            
     def get_name(self):
         return self.tif.get_type_name()
 
     def get_size(self):
-        #4 dwords (16 bytes)
         return self.tif.get_unpadded_size()
 
-def parse_pcd(pcd_addr, pcd_struct):
+class PCDStruct(CStruct): 
+    tif = 0
+    PCD_BASE_IDA_STRUCT_TYPE_NAME = "ProtConfDescriptor"
+    SELFREF = REFINFO_SIGNEDOP | REFINFO_SELFREF
 
+    def _create_pcd_struct(self, pcd_address):
+        self.tif = ida_typeinf.tinfo_t()
+        udt = ida_typeinf.udt_type_data_t()
+        self.tif.create_udt(udt)
+        self.tif.set_named_type(None, self.PCD_BASE_IDA_STRUCT_TYPE_NAME+"_"+hex(pcd_address))
+        self.append_reloffset_member("ProtocolDescriptor")
+        self.append_reloffset_member("NominalTypeDescriptor")
+        #If PWT field creates a reloffset to itself (PWT dword = 0) it means it doesn't have PWT.
+        self.append_reloffset_member("PWT")
+        self.append_int32_member("Flags")
+
+    def __init__(self, pcd_address):  
+        self._create_pcd_struct(pcd_address)
+
+class RelWitnessTableStruct(CStruct):
+    tif = 0
+    RWT_BASE_IDA_STRUCT_TYPE_NAME = "RelWitTable"
+    SELFREF = REFINFO_SIGNEDOP | REFINFO_SELFREF
+
+    def _create_rwt(self, rwt_addr, rel_wit_num):
+        self.tif = ida_typeinf.tinfo_t()
+        udt = ida_typeinf.udt_type_data_t()
+        self.tif.create_udt(udt)
+        self.tif.set_named_type(None, self.RWT_BASE_IDA_STRUCT_TYPE_NAME+"_"+hex(rwt_addr))
+        self.append_int32_member("NumWitnesses")
+        for idx in range(rel_wit_num):
+            req_addr = rwt_addr+(idx*2*4)
+            req_name = get_symbol_name_from_address(req_addr)
+            self.append_reloffset_member("req_"+req_name)
+            self.append_reloffset_member("impl_"+req_name)
+
+    def __init__(self, rwt_addr):
+        rel_wit_num = get_word_with_size(rwt_addr, 4)
+        rwt_addr += 4
+        self._create_rwt(rwt_addr, rel_wit_num)
+
+class GenWitnessTableStruct(CStruct):
+    tif = 0
+    GWT_BASE_IDA_STRUCT_TYPE_NAME = "GenWitTable"
+    SELFREF = REFINFO_SIGNEDOP | REFINFO_SELFREF
+
+    def _create_gwt(self, gwt_addr):
+        self.tif = ida_typeinf.tinfo_t()
+        udt = ida_typeinf.udt_type_data_t()
+        self.tif.create_udt(udt)
+        self.tif.set_named_type(None, self.GWT_BASE_IDA_STRUCT_TYPE_NAME+"_"+hex(gwt_addr))
+        self.append_int16_member("WitnessTableSizeInWords")
+        self.append_int16_member("WitnessTablePrivateSizeInWords")
+        #If Instantiator field creates a reloffset to itself (Instantiator dword = 0) it means it doesn't have Instantiator.
+        self.append_reloffset_member("Instantiator")
+        self.append_reloffset_member("PrivateData")
+
+    def __init__(self, gwt_addr):
+        self._create_gwt(gwt_addr)
+
+def get_symbol_name_from_address(ea):
+    requirement_offset = get_word_with_size(ea, 4)
+    requirement_addr = ea + ( ((requirement_offset & 0xffffffff)^0x80000000)-0x80000000 )
+    definition_addr = get_word_with_size(requirement_addr-1, 8)
+    name = ida_name.get_name(definition_addr)
+    try:
+        return idc.demangle_name(name, idc.get_inf_attr(idc.INF_SHORT_DN)) 
+    except Exception as e:
+        #Could not demangle the symbol. Just return the name
+        return str(requirement_addr)
+
+def get_word_with_size(ea, size):
+    if(size == 4):
+        return ida_bytes.get_wide_dword(ea)
+    elif(size == 8):
+        return ida_bytes.get_qword(ea)
+
+def create_word_with_size(ea, size):
+    if(size == 2):
+        ida_bytes.create_word(ea, 2)
+    elif(size == 4):
+        ida_bytes.create_dword(ea, 4)
+
+def make_offset(ea):
+    idc.op_offset(ea, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, ea, 0)
+
+def parse_pcd(pcd_addr):
+
+    pcd_struct = PCDStruct(pcd_addr)
     idc.create_struct(pcd_addr, pcd_struct.get_size(), pcd_struct.get_name())
-
-    idc.op_offset(pcd_addr, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, pcd_addr, 0)
-    idc.op_offset(pcd_addr+4, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, pcd_addr+4, 0)
     
-    pwt_val = idc.get_wide_dword(pcd_addr+8)
-    if pwt_val>0:
-        #create the offset reference only if it is defined
-        idc.op_offset(pcd_addr+8, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, pcd_addr+8, 0)
-    
-    flags_value = idc.get_wide_dword(pcd_addr+12)
+    flags_value = get_word_with_size(pcd_addr+12, 4)
 
     if flags_value not in (0x30000, 0x300c0, 0x30080):
         print("Unsupported flag value: ",hex(flags_value),". Not parsing this... (WIP!)")
         return
 
-    ida_bytes.create_dword(pcd_addr+16, 4)
     base_relwitness_addr = 0
     flag_mask_res = flags_value & 0x000c0
     if flag_mask_res == 0xc0:
+        #Create Retroactive string reference
+        create_word_with_size(pcd_addr+16, 4)
         idc.set_cmt(pcd_addr+16, "Retroactive string", 0)
-        idc.op_offset(pcd_addr+16, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, pcd_addr+16, 0)
+        make_offset(pcd_addr+16)
         base_relwitness_addr = pcd_addr + 20
     else:
         base_relwitness_addr = pcd_addr + 16
 
-    ida_bytes.create_dword(base_relwitness_addr, 4)
-    numWitnesses = idc.get_wide_dword(base_relwitness_addr)
-    idc.set_cmt(base_relwitness_addr, "NumWitnesses", 0)
-    base_relwitness_addr += 4
+    #Relative Witness Table casting
+    rwt_struct = RelWitnessTableStruct(base_relwitness_addr)
+    idc.create_struct(base_relwitness_addr, rwt_struct.get_size(), rwt_struct.get_name())
+
+    base_gwt_addr = base_relwitness_addr +  rwt_struct.get_size()
     
-    for idx in range(numWitnesses):
-        #create the rel offsets to their definitions
-        ida_bytes.create_dword(base_relwitness_addr, 4)
-        idc.op_offset(base_relwitness_addr, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, base_relwitness_addr, 0)
-        requirement_offset = idc.get_wide_dword(base_relwitness_addr)
-        requirement_addr = base_relwitness_addr + ( ((requirement_offset & 0xffffffff)^0x80000000)-0x80000000 )
-        definition_addr = idc.get_qword(requirement_addr-1)
-        name = ida_name.get_name(definition_addr)
-        idc.set_cmt(base_relwitness_addr, idc.demangle_name(name, idc.get_inf_attr(idc.INF_SHORT_DN)), 0)
-        ida_bytes.create_dword(base_relwitness_addr+4, 4)
-        idc.op_offset(base_relwitness_addr+4, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, base_relwitness_addr+4, 0)
-        base_relwitness_addr += 8
-
-    base_generic = base_relwitness_addr
-    ida_bytes.create_word(base_generic, 2)
-    idc.set_cmt(base_relwitness_addr, "Witness table size in words", 0)
-    ida_bytes.create_word(base_generic+2, 2)
-    idc.set_cmt(base_generic+2, "Witness table private size and requires instantiation", 0)
-    ida_bytes.create_dword(base_generic+4, 4)
-    idc.set_cmt(base_generic+4, "Instantiator", 0)
-    ida_bytes.create_dword(base_generic+8, 4)
-    idc.set_cmt(base_generic+8, "Private data", 0)
-    instantiator = idc.get_wide_dword(base_generic+4)
-    if instantiator > 0:
-        idc.op_offset(base_generic+4, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, base_generic+4, 0)
-
-    idc.op_offset(base_generic+8, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, base_generic+8, 0)
+    #Generic Witness Table casting
+    gwt_struct = GenWitnessTableStruct(base_gwt_addr)
+    idc.create_struct(base_gwt_addr, gwt_struct.get_size(), gwt_struct.get_name())
 
 def main():
     seg = None
@@ -137,13 +175,12 @@ def main():
     min_addr = seg.start_ea
     max_addr = seg.end_ea
 
-    pcd_struct = PCDStruct()
     for curr_addr in range(min_addr, max_addr, 4):
         ida_bytes.create_dword(curr_addr, 4)
         idc.op_offset(curr_addr, 0, REF_OFF32|REFINFO_SIGNEDOP, -1, curr_addr, 0)
         offset = idc.get_wide_dword(curr_addr)
         pcd_addr = curr_addr + ( ((offset & 0xffffffff)^0x80000000)-0x80000000 )
-        parse_pcd(pcd_addr, pcd_struct)
+        parse_pcd(pcd_addr)
 
 if __name__=="__main__":
     main()
